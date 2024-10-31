b8d32ced6e66dbfbed1ef070a1346328914edbc7
Franklin Ferreira <franklinferreira280@gmail.com>
2024-10-31 12:34:45 -0300

    adicionando tela menores

--- loja_rural/script.js
+++ loja_rural/script.js
@@ -1,3 +1,15 @@
+// Detecta o tamanho da tela e carrega o CSS correspondente
+const screenWidth = window.innerWidth;
+
+if (screenWidth <= 768) {
+  document.write('<link rel="stylesheet" href="css/pequena/style.css">');
+} else if (screenWidth > 768 && screenWidth <= 1024) {
+  document.write('<link rel="stylesheet" href="css/media/style.css">');
+} else {
+  document.write('<link rel="stylesheet" href="css/grande/style.css">');
+}
+
+
 document.querySelectorAll('.quantity-btn').forEach(button => {
     button.addEventListener('click', function() {
         const input = this.parentNode.querySelector('.quantity-input');
